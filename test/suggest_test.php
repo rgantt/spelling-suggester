a048ecb32dbe684deb6cdd370b8c2e78a44add4d
Ryan <ryan@zuttonet.com>
2011-09-11 23:29:49 -0600

    using the fact that we can train on strings now

--- test/suggest_test.php
+++ test/suggest_test.php
@@ -2,23 +2,24 @@
 require_once dirname(__FILE__).'/../suggest.php';
 
 class suggest_test extends \PHPUnit_Framework_TestCase {
-	/**
-	 * Can't use setUp/tearDown because __invoke doesn't work with $this->
-	 */
-	private function init() {
-		return new Suggest(
-			array( 'fun', 'fun', 'fun', 'gun', 'gun', 'sun', 'run' )
-		);
+	private $s;
+	
+	public function setUp() {
+		$this->s = new Suggest("fun fun fun gun gun sun run");
+	}
+	
+	public function tearDown() {
+		unset( $this->s );
 	}
 	
     public function test_suggests_highest_probability_word() {
-        $s = $this->init();
+        $s = $this->s;
         $this->assertEquals( "fun", $s("kun") );
 		$this->assertEquals( "fun", $s("iun") );
     }
 	
 	public function test_suggests_known_word_instead_of_probabilistic_word() {
-		$s = $this->init();
+		$s = $this->s;
 		$this->assertEquals( "run", $s("run") );
 		$this->assertEquals( "sun", $s("sun") );
 		$this->assertEquals( "gun", $s("gun") );
