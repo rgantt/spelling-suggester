f6790120edf79618da214970269b8620facc588a
Ryan <ryan@zuttonet.com>
2011-09-12 19:09:39 -0600

    need to check frequency array since we aren't saving a raw words array anymore

--- test/wordtrainer_test.php
+++ test/wordtrainer_test.php
@@ -14,16 +14,16 @@ class wordtrainer_test extends \PHPUnit_Framework_TestCase {
 	
     public function test_can_train_with_string() {
         $w = new WordTrainer("this is a string of words");
-		$this->assertTrue( in_array( "string", $w->getWords() ) );
-		$this->assertTrue( in_array( "this", $w->getWords() ) );
-		$this->assertTrue( in_array( "words", $w->getWords() ) );
+		$this->assertTrue( in_array( "string", array_keys( $w->getFrequencies() ) ) );
+		$this->assertTrue( in_array( "this", array_keys( $w->getFrequencies() ) ) );
+		$this->assertTrue( in_array( "words", array_keys( $w->getFrequencies() ) ) );
     }
 	
 	public function test_can_train_with_array() {
 		$w = new WordTrainer( array( "this", "is", "a", "string", "of", "words" ) );
-		$this->assertTrue( in_array( "string", $w->getWords() ) );
-		$this->assertTrue( in_array( "this", $w->getWords() ) );
-		$this->assertTrue( in_array( "words", $w->getWords() ) );
+		$this->assertTrue( in_array( "string", array_keys( $w->getFrequencies() ) ) );
+		$this->assertTrue( in_array( "this", array_keys( $w->getFrequencies() ) ) );
+		$this->assertTrue( in_array( "words", array_keys( $w->getFrequencies() ) ) );
 	}
 	
 	public function test_correct_frequency_calculation() {
